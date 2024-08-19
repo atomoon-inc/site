5a73ec5d53ad2329c3792c7605306486118d779d
Jordan Otávio da Silva <danotavio@gmail.com>
2024-08-19 12:51:53 -0300

    add netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import react from "@astrojs/react";
 import { defineConfig } from "astro/config";
 
+import netlify from "@astrojs/netlify";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  build: {
-    format: "file",
-  },
+  output: "server",
+  adapter: netlify({ edgeMiddleware: true }),
   integrations: [react(), tailwind({ nesting: true })],
 });
